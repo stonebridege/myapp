8f7fae321d606543dc9aa59047b8f5c608247c64
stonebridge <stonebridge@njfu.edu.cn>
2023-04-19 14:55:34 -0700

    5.1.2.setState修改状态

---
+++ src/01-base/08-state.js
@@ -0,0 +1,29 @@
+import React, {Component} from "react";
+
+export default class App extends Component {
+    //state变量名是固定的
+    state = {
+        myText: "收藏",
+        isCollect: false
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>欢迎来到react开发</h1>
+                <button onClick={this.collectFile}>{this.state.isCollect ? "收藏" : '取消收藏'}</button>
+            </div>
+        )
+    }
+
+    collectFile = () => {
+        this.setState({
+            isCollect: !this.state.isCollect
+        })
+        if (this.state.isCollect) {
+            console.log("完成收藏的逻辑，给后端发送信息")
+        } else {
+            console.log("完成取消收藏的逻辑，给后端发送信息")
+        }
+    }
+}
