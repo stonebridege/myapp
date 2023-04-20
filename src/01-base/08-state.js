b39703c726e18897dfafb7851e726cde35a2720b
stonebridge <stonebridge@njfu.edu.cn>
2023-04-19 19:42:16 -0700

    5.1.2.setState修改状态方式2

--- src/01-base/08-state.js
+++ src/01-base/08-state.js
@@ -2,15 +2,22 @@ import React, {Component} from "react";
 
 export default class App extends Component {
     //state变量名是固定的
-    state = {
-        myText: "收藏",
-        isCollect: false
+    // state = {
+    //     myText: "收藏",
+    //     isCollect: false
+    // }
+    constructor() {
+        super()
+        this.state = {
+            isCollect: false,
+            userName: "stonebridge"
+        }
     }
 
     render() {
         return (
             <div>
-                <h1>欢迎来到react开发</h1>
+                <h1>欢迎来到react开发-{this.state.userName}</h1>
                 <button onClick={this.collectFile}>{this.state.isCollect ? "收藏" : '取消收藏'}</button>
             </div>
         )
@@ -18,7 +25,8 @@ export default class App extends Component {
 
     collectFile = () => {
         this.setState({
-            isCollect: !this.state.isCollect
+            isCollect: !this.state.isCollect,
+            userName: 'zhangsan'
         })
         if (this.state.isCollect) {
             console.log("完成收藏的逻辑，给后端发送信息")
