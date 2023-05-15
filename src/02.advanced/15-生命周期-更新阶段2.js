4a655cfdd97908b357c7d7f74cd33b6edb87fbb5
stonebridge <stonebridge@njfu.edu.cn>
2023-05-14 21:30:55 -0700

    9.2.2.shouldComponentUpdate

---
+++ src/02.advanced/15-生命周期-更新阶段2.js
@@ -0,0 +1,45 @@
+import React, {Component} from 'react';
+
+export default class App extends Component {
+    state = {
+        myName: "kerwin"
+    }
+
+    render() {
+        console.log("render")
+        return (
+            <div>
+                <button onClick={() => {
+                    this.setState({
+                        myName: "stonebridge"
+                    })
+                }}>click
+                </button>
+
+                {this.state.myName}
+            </div>
+        );
+    }
+
+    /**
+     * 在这里进行判断，如果老状态不不同于新状态就更新。减少无效的虚拟dom的对比，提高react的性能，
+     * @param nextProps :将更新的 props
+     * @param nextState :将更新的 state，老状态来自this.state
+     * @param nextContext
+     */
+    shouldComponentUpdate(nextProps, nextState, nextContext) {
+        if (JSON.stringify(this.state) !== JSON.stringify(nextState)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    UNSAFE_componentWillUpdate(nextProps, nextState, nextContext) {
+        console.log("UNSAFE_componentWillUpdate")
+    }
+
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        console.log("componentDidUpdate")
+    }
+}
