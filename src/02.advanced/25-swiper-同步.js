bacac3085a38e5a376adc75eee6a4000a027dbdf
stonebridge <stonebridge@njfu.edu.cn>
2023-05-22 10:11:55 -0700

    9.7.1.同步swiper

--- src/02.advanced/25-swiper-同步.js
+++ src/02.advanced/25-swiper-同步.js
@@ -1,14 +1,19 @@
 import React, {Component} from 'react';
+//1.引入Swiper核心组件和Navigation
 import Swiper, {Navigation, Pagination} from "swiper";
+//2.引入Swiper的css
 import 'swiper/swiper-bundle.css'
-
+//3.使用 Swiper库或插件Navigation，Pagination
 Swiper.use([Navigation, Pagination])
 
 export default class App extends Component {
+    //4.此时数据是固定的
     state = {
         list: ["111", "222", "333"]
     }
 
+    //5.组件挂载到DOM上之后所执行的componentDidMount函数
+    //初始化swiper对象(className=swiper)，并设置pagination分页（className=swiper-pagination）
     componentDidMount() {
         new Swiper('.swiper', {
             pagination: {
@@ -20,6 +25,7 @@ export default class App extends Component {
     render() {
         return (
             <div>
+                {/*6.html构造要符合官网要求*/}
                 <div className="swiper">
                     <div className="swiper-wrapper" style={{height: '200px', background: 'yellow'}}>
                         {
@@ -28,13 +34,6 @@ export default class App extends Component {
                     </div>
                     {/*如果需要分页器 */}
                     <div className="swiper-pagination"></div>
-
-                    {/*如果需要导航按钮*/}
-                    {/*<div className="swiper-button-prev"></div>*/}
-                    {/*<div className="swiper-button-next"></div>*/}
-
-                    {/*如果需要滚动条*/}
-                    {/*<div className="swiper-scrollbar"></div>*/}
                 </div>
             </div>
         );
