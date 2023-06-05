1c8477c7fd8aa7cc1436f7ac42ef70c5c781b10b
stonebridge <stonebridge@njfu.edu.cn>
2023-06-05 13:16:56 -0700

    index.js针对react18优化

--- src/index.js
+++ src/index.js
@@ -6,17 +6,11 @@ import ReactDOM from 'react-dom'
 // 例如把123456渲染到root节点上:ReactDOM.render("123456", document.getElementById('root'))，呈现效果：<div id="root">123456</div>
 // ReactDOM.render(<div>123456</div>, document.getElementById('root'))  呈现效果：<div id="root"><div>123456</div></div>
 import {createRoot} from "react-dom/client";
-
-import {BrowserRouter as Router} from "react-router-dom";
-
 import App from "./07-react-redux/App";
 import store from "./07-react-redux/redux/store";
 import {Provider} from "react-redux";
 
-ReactDOM.render(<Provider store={store}><App/></Provider>, document.getElementById('root'))
 
-//检测redux中任何状态的改变，如果redux的状态发生了改变，那么重新渲染App组件,
-// 使用react-redux，不用在index.js里手动监测redux里state的变化，容器组件可以监测到redux里state的变化。
-// store.subscribe(() => {
-//     ReactDOM.render(<App/>, document.getElementById('root'))
-// })
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(<Provider store={store}><App/></Provider>)
