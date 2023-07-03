5a37048c64e632b58219a0db3d06d942be42e4b4
stonebridge <stonebridge@njfu.edu.cn>
2023-07-02 18:15:36 -0700

    3.2.2.生命周期流程图（旧）更新过程

--- src/08-life/Lifecycle01.jsx
+++ src/08-life/Lifecycle01.jsx
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
 
 export default class App extends Component {
     /**
@@ -12,7 +13,7 @@ export default class App extends Component {
     }
 
     //组件将要挂载时调用的生命周期回调函数:componentWillMount
-    UNSAFE_componentWillMount () {
+    UNSAFE_componentWillMount() {
         console.log("Lifecycle01.jsx--->componentWillMount")
     }
 
@@ -26,15 +27,44 @@ export default class App extends Component {
         const {count} = this.state
         this.setState({count: count + 1})
     }
+    //death按钮的组件
+    death = () => {
+        const targetNode = document.getElementById('test');
+        ReactDOM.unmountComponentAtNode(targetNode);
+        targetNode.remove();
+    }
+
+    componentWillUnmount() {
+        console.log("Lifecycle01.jsx--->componentWillUnmount")
+    }
+
+    //控制组件更新的”阀门“
+    shouldComponentUpdate() {
+        console.log("Lifecycle01.jsx--->shouldComponentUpdate")
+        return true
+    }
+
+    //组件将要更新的钩子函数
+    componentWillUpdate() {
+        console.log("Lifecycle01.jsx--->componentWillUpdate")
+    }
+
+    //组件更新完毕的狗子
+    componentDidUpdate() {
+        console.log("Lifecycle01.jsx--->componentDidUpdate")
+    }
 
     //render()函数
     render() {
         console.log("Lifecycle01.jsx--->render")
         const {count} = this.state
         return (
-            <div>
-                <h2>当前求和为：{count}</h2>
-                <button onClick={this.add}>点我+1</button>
+            <div id='test'>
+                <div>
+                    <h2>当前求和为：{count}</h2>
+                    <button onClick={this.add}>点我+1</button>
+                    <button onClick={this.death}>卸载组件</button>
+                </div>
             </div>
         );
     }
